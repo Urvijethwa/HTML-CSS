3d5837568d09652a5fde393dda964415dbd1f1ba
Urvijethwa <urvijethwa00@gmail.com>
2025-09-06 20:05:13 +0100

    array dsa

---
+++ Arrays.java
@@ -0,0 +1,26 @@
+import java.util.*; 
+public class Arrays{
+    public static void main(String args[]){
+        int marks[] = new int[20];   //arrays are static 
+
+        // Scanner sc = new Scanner(System.in); 
+        // System.out.print("enter physics marks: ");
+        // int phy = sc.nextInt(); 
+        // System.out.println(phy + 2); 
+
+        // System.out.println("enter chemistry marks: ");
+        // marks[1] = sc.nextInt(); 
+        // System.out.print(marks[1]);
+
+        // System.out.println("enter biology marks: ");
+        // marks[2] = sc.nextInt(); 
+        // System.out.println(marks[2]);
+
+        // //percentage 
+        // int per = (phy + marks[1] + marks[2])/3; 
+        // System.out.println("percentage : " + per + "%");
+
+        //lenght 
+        System.out.println(marks.length);
+    }
+}
