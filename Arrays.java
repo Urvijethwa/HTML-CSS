eac61e12d4c48dc56742aa5f0da6d210b06a0bc7
Urvijethwa <156341000+Urvijethwa@users.noreply.github.com>
2025-09-07 11:56:17 +0100

    linear search

--- Arrays.java
+++ Arrays.java
@@ -1,26 +1,19 @@
-import java.util.*; 
 public class Arrays{
+
+    //linear search - return the index
+    public static int linear(int arr[], int key){
+        for(int i=0; i<=arr.length; i++){
+            if (arr[i] == key) {
+                return i; 
+            }
+        }
+        return -1; //if does not exist 
+    }
+
     public static void main(String args[]){
-        int marks[] = new int[20];   //arrays are static 
+        int arr[] = {2, 5, 8, 2, 4}; 
+        int key = 8; 
 
-        // Scanner sc = new Scanner(System.in); 
-        // System.out.print("enter physics marks: ");
-        // int phy = sc.nextInt(); 
-        // System.out.println(phy + 2); 
-
-        // System.out.println("enter chemistry marks: ");
-        // marks[1] = sc.nextInt(); 
-        // System.out.print(marks[1]);
-
-        // System.out.println("enter biology marks: ");
-        // marks[2] = sc.nextInt(); 
-        // System.out.println(marks[2]);
-
-        // //percentage 
-        // int per = (phy + marks[1] + marks[2])/3; 
-        // System.out.println("percentage : " + per + "%");
-
-        //lenght 
-        System.out.println(marks.length);
+       System.out.println(linear(arr, key));
     }
 }
